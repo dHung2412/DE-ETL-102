c3686066ae21c22569dec4596d81899100b1cced
dHung2412 <148836286+dHung2412@users.noreply.github.com>
2025-03-22 00:11:42 +0700

    Delete Day1 directory

--- Day1/RDD/Actions/take.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkContext
-
-sc = SparkContext("local[*]", appName="DE-ETL-02")
-
-data = [ 1,2,3,4,5,6,7,8]
-
-rdd = sc.parallelize(data)
-
-print(rdd.take(10))
--- Day1/RDD/Transformation/FileDataSource.py
+++
@@ -1,8 +0,0 @@
-from pyspark import SparkContext, SparkConf
-
-conf = SparkConf().setAppName("DE-ETL-02").setMaster("local[*]").set("spark.executor.memory", "2g")
-sc = SparkContext(conf=conf)
-
-fileRDD = sc.textFile("D:\Project File\Pycharm\PythonProject4\Data\da1")
-
-print(fileRDD.collect())
